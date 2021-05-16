62d453c9368279b33e5a1cad972deef8732317d7
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-05-16 15:07:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 120;
+        int oneMile = 20;
+        int countMiles = price / oneMile;
+        System.out.println("Ваши мили: " + countMiles);
+    }
+}
