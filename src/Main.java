26a3f7c47328f3f01fd06c462f0da418763b3963
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-05-18 21:40:46 +0300

    Add kopecks

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 120;
-        int oneMile = 20;
+        int price = 120_50;
+        int oneMile = 20_00;
         int countMiles = price / oneMile;
         System.out.println("Ваши мили: " + countMiles);
     }
 }
+ 
